0581b63587f2b5217a775cf2f460205987470c8d
Kyle Jennings <kylejennings@kyles-MacBook-Air.local>
2025-07-27 21:31:07 -0400

    built game for public

---
+++ src/Game.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import DevPanel from '@/components/DevPanel.vue';
+import GameCanvas from '@/components/GameCanvas.vue';
+
+</script>
+
+<template>
+  <div id="app">
+    <div class="is-flex is-justify-content-space-between">
+      <game-canvas />
+    </div>
+  </div>
+</template>
---
+++ src/game-main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import Game from '@/components/GameCanvas.vue';
+
+const app = createApp(Game);
+app.mount('#app');
