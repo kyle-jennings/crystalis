d9f5f383c3572c320cb72298caad98df5ce07810
Kyle Jennings <kylejennings@kyles-MacBook-Air.local>
2025-07-28 10:16:00 -0400

    working on game build

--- src/game/entry.js
+++ src/game/entry.js
@@ -1,6 +1,7 @@
 import CrystalisGame from "./js/CrystalisGame";
 // Start the game when page loads
 window.addEventListener('load', () => {
+  console.log('load the game!');
   window.CrystalisGame = new CrystalisGame({
     $elm: '#app',
   });
